7a92d0e77192b048ce8b0ba1b7fe0333530fe048
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-05-06 08:04:58 +0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,1 +1,15 @@
+def count_uppercase() -> int:
+    """
+    Counts uppercase letters in srez from i to j
+    Returns:
+        int: number of uppercase letters
+    """
+    s = input().strip()
+    i, j = map(int, input().split())
+    srez = s[i-1:j]
+    result = list(filter(lambda x: x.isupper(), srez)
+    return len(result)
 
+
+if __name__ == "__main__":
+    print(count_uppercase())
