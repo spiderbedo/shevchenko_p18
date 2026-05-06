3db4a01ddff196fbd3e30122d715ac0bd839a695
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-05-06 08:15:10 +0700

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,1 +1,16 @@
+def sum_multiples() -> int:
+  
+    """
+    Finds the sum of numbers on [a; b] that are divisible by c and d.
+    Returns:
+        int: sum of suitable numbers.
+    """
+  
+    a, b, c, d = map(int, input().split())
+    numbers = range(a, b + 1)
+    filtered = filter(lambda x: x % c == 0 and x % d == 0, numbers)
+    return sum(filtered)
 
+
+if __name__ == "__main__":
+    print(sum_multiples())
