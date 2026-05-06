5633000d0a309e58ec75cfac6ac282638d073fd7
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-05-06 08:21:43 +0700

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,1 +1,19 @@
+import json
 
+def sort_json_list() -> list:
+
+    """
+    Sorts a list of lists in descending order by the numeric value
+    of the second element.
+    Returns:
+        list: sorted list.
+    """
+
+    data = input().strip()
+    lst = json.loads(data)
+    lst.sort(key=lambda x: x[1], reverse=True)
+    return lst
+
+
+if __name__ == "__main__":
+    print(sort_json_list())
